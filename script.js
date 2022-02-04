eaae39efe1ec72f534b887bb0b529aa22c5664dd
leuchtend5 <zxcmj1@gmail.com>
2022-02-05 02:48:12 +0800

    Edit js

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ const currentScreen = document.getElementById('current');
 let currentOperation = null;
 let firstNumber = "";
 let secondNumber = "";
-let resetScreen = false;
 
 clearBtn.addEventListener('click', clearNumber);
 deleteBtn.addEventListener('click', deleteNumber);
-equalBtn.addEventListener('click', () => {
+equalBtn.addEventListener('click', () => { //for equal button
   if(currentOperation == null){
     return;
   }
   execute(equalBtn.textContent);
 })
+window.addEventListener('keydown', keyboardInput);
 
 numberBtn.forEach((button) => {
   button.addEventListener('click', () => addNumber(button.textContent));
@@ -29,24 +29,40 @@ operatorBtn.forEach((button) => {
   button.addEventListener('click', () => setOperation(button.textContent));
 })
 
+// collect all the input number
 function addNumber(number){
-  if(currentScreen.textContent === "0"){
+  if(currentOperation == "÷" && number == "0"){
+    alert("you cannot divide by 0 !");
+    number = "";
+  } else if(currentScreen.textContent === "0"){
     currentScreen.textContent = "";
-  } else if(resetScreen){
-    currentScreen.textContent = "";
-    resetScreen = false;
   }
   currentScreen.textContent += number;
 }
 
+function keyboardInput(e){
+  if(e.key <= 9){
+    addNumber(e.key);
+  } else if(e.key == "+" || e.key == "-"){
+    setOperation(e.key);
+  } else if(e.key == "/"){
+    setOperation("÷");
+  } else if(e.key == "*"){
+    setOperation("x");
+  }
+  return;
+}
+
+// set the operator
 function setOperation(operator){
+  // this is what will happen when user already input the operation before
   if(currentOperation !== null){
     execute(operator);
-  } else if(currentOperation === null){
+  } else if(currentOperation === null){ // when there is no operation, this logic will run
     firstNumber = currentScreen.textContent;
     currentOperation = operator;
-    historyScreen.textContent = `${firstNumber} ${currentOperation}`
-    resetScreen = true; // for the screen to reset when enter the number
+    historyScreen.textContent = `${firstNumber} ${currentOperation}`;
+    currentScreen.textContent = "";
   } else{
     return;
   }
@@ -54,25 +70,27 @@ function setOperation(operator){
 
 function execute(operator){
   secondNumber = currentScreen.textContent;
-  if(operator == "="){
-    currentScreen.textContent = operate(currentOperation, firstNumber, secondNumber);
+  if(secondNumber == ""){
+    historyScreen.textContent = `${firstNumber} ${operator}`
+    currentOperation = operator;
+    return;
+  } else if(operator == "=" && secondNumber !== ""){
+    currentScreen.textContent = parseFloat(operate(currentOperation, firstNumber, secondNumber).toFixed(3));
     historyScreen.textContent = `${firstNumber} ${currentOperation} ${secondNumber} =`;
     currentOperation = null;
     firstNumber = currentScreen.textContent;
     secondNumber = "";
-  } // else if(operator == "+" || operator == "-" || operator == "÷" || operator == "*"){
-    // currentOperation = operator;
-    // historyScreen.textContent = `${firstNumber} ${currentOperation}`;
-    // resetScreen = true;
-  // } 
-  else{
-    currentScreen.textContent = operate(operator, firstNumber, secondNumber);
+  } else if(historyScreen.textContent.includes("+") || // this logic will calculate the previous operator then show current operator
+            historyScreen.textContent.includes("-") || // e.g previous operator +, then user input -, this login will calculate the + first, then show the result with operator -
+            historyScreen.textContent.includes("÷") ||
+            historyScreen.textContent.includes("x") && secondNumber !== ""){
+    currentScreen.textContent = parseFloat(operate(currentOperation, firstNumber, secondNumber).toFixed(3));
     historyScreen.textContent = `${currentScreen.textContent} ${operator}`;
     firstNumber = currentScreen.textContent;
     secondNumber = "";
-    resetScreen = true;
+    currentOperation = operator;
+    currentScreen.textContent = "";
   }
-  
 }
 
 function operate(operator, a, b){
