c9af45083ee638b684e37b0e5592c64abe0e6bc7
leuchtend5 <zxcmj1@gmail.com>
2022-02-06 13:33:00 +0800

    Commit all files

--- script.js
+++ script.js
@@ -1,90 +1,136 @@
-const clearBtn = document.getElementById('clear');
-const deleteBtn = document.getElementById('delete');
-const pointBtn = document.getElementById('point');
-const equalBtn = document.getElementById('equal');
-const numberBtn = document.querySelectorAll('.number');
-const operatorBtn = document.querySelectorAll('.operator');
-const historyScreen = document.getElementById('history');
-const currentScreen = document.getElementById('current');
+const clearBtn = document.getElementById("clear");
+const deleteBtn = document.getElementById("delete");
+const pointBtn = document.getElementById("point");
+const equalBtn = document.getElementById("equal");
+const numberBtn = document.querySelectorAll(".number");
+const operatorBtn = document.querySelectorAll(".operator");
+const historyScreen = document.getElementById("history");
+const currentScreen = document.getElementById("current");
 
 let currentOperation = null;
 let firstNumber = "";
 let secondNumber = "";
 
-clearBtn.addEventListener('click', clearNumber);
-deleteBtn.addEventListener('click', deleteNumber);
-equalBtn.addEventListener('click', () => { //for equal button
-  if(currentOperation == null){
+clearBtn.addEventListener("click", clearNumber);
+deleteBtn.addEventListener("click", deleteNumber);
+equalBtn.addEventListener("click", () => {
+  //for equal button
+  if (currentOperation == null) {
     return;
   }
   execute(equalBtn.textContent);
-})
-window.addEventListener('keydown', keyboardInput);
+});
+pointBtn.addEventListener("click", (e) => pointButton(e));
+window.addEventListener("keydown", keyboardInput);
 
 numberBtn.forEach((button) => {
-  button.addEventListener('click', () => addNumber(button.textContent));
-})
+  button.addEventListener("click", () => addNumber(button.textContent));
+});
 
 operatorBtn.forEach((button) => {
-  button.addEventListener('click', () => setOperation(button.textContent));
-})
+  button.addEventListener("click", () => setOperation(button.textContent));
+});
+
+function pointButton(e) {
+  if (currentScreen.textContent.includes(".")) {
+    return;
+  } else if (currentScreen.textContent == "") {
+    return;
+  } else {
+    currentScreen.textContent += ".";
+  }
+}
 
 // collect all the input number
-function addNumber(number){
-  if(currentOperation == "÷" && number == "0"){
+function addNumber(number) {
+  if (
+    currentOperation == "÷" &&
+    number == "0" &&
+    currentScreen.textContent.length == 0
+  ) {
     alert("you cannot divide by 0 !");
     number = "";
-  } else if(currentScreen.textContent === "0"){
+  } else if (currentScreen.textContent === "0") {
     currentScreen.textContent = "";
+  } else if (currentScreen.textContent.length == 12) {
+    return;
   }
   currentScreen.textContent += number;
 }
 
-function keyboardInput(e){
-  if(e.key <= 9){
+function keyboardInput(e) {
+  if (e.key <= 9) {
     addNumber(e.key);
-  } else if(e.key == "+" || e.key == "-"){
-    setOperation(e.key);
-  } else if(e.key == "/"){
-    setOperation("÷");
-  } else if(e.key == "*"){
-    setOperation("x");
   }
-  return;
+
+  switch (e.key) {
+    case "+":
+      setOperation("+");
+      break;
+    case "-":
+      setOperation("-");
+      break;
+    case "*":
+      setOperation("x");
+      break;
+    case "/":
+      setOperation("÷");
+      break;
+    case "Enter":
+      execute("=");
+      break;
+    case ".":
+      pointButton(".");
+      break;
+    case "Backspace":
+      deleteNumber();
+      break;
+    default:
+      return;
+  }
 }
 
 // set the operator
-function setOperation(operator){
+function setOperation(operator) {
   // this is what will happen when user already input the operation before
-  if(currentOperation !== null){
+  if (currentOperation !== null) {
     execute(operator);
-  } else if(currentOperation === null){ // when there is no operation, this logic will run
+  } else if (currentOperation === null) {
+    // when there is no operation, this logic will run
     firstNumber = currentScreen.textContent;
     currentOperation = operator;
     historyScreen.textContent = `${firstNumber} ${currentOperation}`;
     currentScreen.textContent = "";
-  } else{
+  } else {
     return;
   }
 }
 
-function execute(operator){
+function execute(operator) {
   secondNumber = currentScreen.textContent;
-  if(secondNumber == ""){
-    historyScreen.textContent = `${firstNumber} ${operator}`
+  if (operator == "=" && currentOperation == null) {
+    return;
+  } else if (secondNumber == "") {
+    historyScreen.textContent = `${firstNumber} ${operator}`;
     currentOperation = operator;
     return;
-  } else if(operator == "=" && secondNumber !== ""){
-    currentScreen.textContent = parseFloat(operate(currentOperation, firstNumber, secondNumber).toFixed(3));
+  } else if (operator == "=" && secondNumber !== "") {
+    currentScreen.textContent = parseFloat(
+      operate(currentOperation, firstNumber, secondNumber).toFixed(3)
+    );
     historyScreen.textContent = `${firstNumber} ${currentOperation} ${secondNumber} =`;
     currentOperation = null;
     firstNumber = currentScreen.textContent;
     secondNumber = "";
-  } else if(historyScreen.textContent.includes("+") || // this logic will calculate the previous operator then show current operator
-            historyScreen.textContent.includes("-") || // e.g previous operator +, then user input -, this login will calculate the + first, then show the result with operator -
-            historyScreen.textContent.includes("÷") ||
-            historyScreen.textContent.includes("x") && secondNumber !== ""){
-    currentScreen.textContent = parseFloat(operate(currentOperation, firstNumber, secondNumber).toFixed(3));
+  } else if (
+    historyScreen.textContent.includes("+") || // this logic will calculate the previous operator then show current operator
+    historyScreen.textContent.includes("-") || // e.g previous operator +, then user input -, this login will calculate the + first, then show the result with operator -
+    historyScreen.textContent.includes("÷") ||
+    (historyScreen.textContent.includes("x") && secondNumber !== "")
+  ) {
+    currentScreen.textContent = parseFloat(
+      operate(currentOperation, firstNumber, secondNumber).toFixed(3)
+    );
     historyScreen.textContent = `${currentScreen.textContent} ${operator}`;
     firstNumber = currentScreen.textContent;
     secondNumber = "";
@@ -93,11 +139,11 @@ function execute(operator){
   }
 }
 
-function operate(operator, a, b){
+function operate(operator, a, b) {
   let computation;
   let previous = parseFloat(a);
   let current = parseFloat(b);
-  switch(operator){
+  switch (operator) {
     case "+":
       computation = previous + current;
       break;
@@ -110,13 +156,13 @@ function operate(operator, a, b){
     case "÷":
       computation = previous / current;
       break;
-    default:        
+    default:
       return;
   }
   return computation;
 }
 
-function clearNumber(){
+function clearNumber() {
   currentScreen.textContent = "0";
   historyScreen.textContent = "";
   currentOperation = null;
@@ -124,7 +170,6 @@ function clearNumber(){
   secondNumber = "";
 }
 
-function deleteNumber(){
+function deleteNumber() {
   currentScreen.textContent = currentScreen.textContent.slice(0, -1);
 }
-
